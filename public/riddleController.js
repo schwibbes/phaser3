af1f7bc0b3875458f5cc20c5d83a556dfdc7922f
dirk <dirk.weber1@googlemail.com>
2021-05-21 23:19:47 +0200

    Fixed respawning of riddles

--- public/riddleController.js
+++ public/riddleController.js
@@ -1,15 +1,21 @@
 function collectRiddle(player, riddle) {
     // Was the solution right or wrong?
-    if (riddle.riddleMode.correct) {
+    if (riddle.riddleModel.correct) {
         updateScore(100);
     } else {
         decreaseLive();
     }
     // In all cases: calculate new riddles 
-    riddles.children,iterate(function(child) {
+    riddles.children.iterate(function(child) {
          child.disableBody(true, true);
     }); 
-    for (let i = 0; i < riddles.children.length; i++) {
-        riddles[i].enableBody(true, newRiddle[i].x, 0, true, true);
-    }
+
+
+    var newRiddle = riddleGenerator.createRiddle();
+    let i = 0;
+    riddles.children.iterate(function(child) {        
+        child.enableBody(true, newRiddle[i].x, 0, true, true);
+        child.riddleModel = newRiddle[i];
+        i++;
+   }); 
 }
