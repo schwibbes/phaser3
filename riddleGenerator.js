8e255b0d2b60c69a3b9e2b505c1a7a242ebdbbb8
dirk <dirk.weber1@googlemail.com>
2021-05-21 21:17:55 +0200

    First version riddle generator

--- riddleGenerator.js
+++ riddleGenerator.js
@@ -5,39 +5,77 @@ var riddleGenerator = (function () {
 		{ question: "buh", answer: "-" },
 	];
 
+	var spawnPoints = [ 30, 70, 100, 300, 500, 700 ];
+
 	function getRandomInt(max) {
 		return Math.floor(Math.random() * max);
 	}
 
 	function getRandomChar(skip) {
 		var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-		var notUniqueAnser = true;
+		var notUniqueAnswer = true;
 
-		while (notUniqueAnser) {
+		while (notUniqueAnswer) {
 			var result = characters.charAt(
-				Math.floor(Math.random() * charactersLength)
+				getRandomInt(characters.length)
 			);
 
 			if (!skip.includes(result)) {
-				notUniqueAnser = false;
+				notUniqueAnswer = false;
 			}
 		}
 
 		return result;
 	}
 
-	function createRiddle() {
-		var answer = solutions[getRandomInt(solutions.length - 1)];
-		var wrong1 = JSON.parse(JSON.stringify(answer));
-		var wrong2 = JSON.parse(JSON.stringify(answer));
+	function getRandomXPos(skip) {
+		var notUniqueAnswer = true;
+
+		while (notUniqueAnswer) {
+			var result = spawnPoints[
+				getRandomInt(spawnPoints.length)
+			];
+
+			if (!skip.includes(result)) {
+				notUniqueAnswer = false;
+			}
+		}	
+		
+		return result;
+	}
+
+	function createRiddleTexts(){
+		var candidate = solutions[getRandomInt(solutions.length - 1)];
+		var answer = JSON.parse(JSON.stringify(candidate));
+		var wrong1 = JSON.parse(JSON.stringify(candidate));
+		var wrong2 = JSON.parse(JSON.stringify(candidate));
 		var charsToSkip = [answer.answer];
 		wrong1.answer = getRandomChar(charsToSkip);
 		wrong2.answer = getRandomChar(charsToSkip.concat(wrong1.answer));
 
-		return {
-			correct: answer,
-			wrong1: wrong1,
-			wrong2: wrong2,
-		};
+		return [ {
+			riddle: answer,
+			correct: true
+		}, {
+			riddle: wrong1,
+			correct: false
+		} , {
+		riddle: wrong1,
+		correct: false
+		}];
 	}
+
+	return {
+		createRiddle() {
+			var riddles = createRiddleTexts();
+			var alreadyUsed = [];
+			for (var i=0; i < 3; i++) {
+				var xPos = getRandomXPos(alreadyUsed);
+				alreadyUsed.push(xPos);
+				riddles[i].x = xPos;
+			}
+			return riddles;
+		}
+	};
 })();
+
