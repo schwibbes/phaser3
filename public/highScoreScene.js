40ad92f7bb44aba09fe906d1843916979a00ddda
Temari <tatarlieva@gmail.com>
2021-05-21 20:54:33 +0200

    Structure js files
    Added scenes.

--- public/highScoreScene.js
+++ public/highScoreScene.js
@@ -1,2 +1,27 @@
-export class HighScoreScene extends Phaser.Scene {
+class HighScoreScene extends Phaser.Scene {
+
+    constructor() {
+        super('highScore');
+    }
+
+    preload ()
+    {
+        this.load.audio('theme', [
+            'sounds/ost-v1.wav'
+        ]);
+
+        //this.load.image('gras-s', 'assets/gras-s.png');
+    }
+
+    create ()
+    {
+        this.sound.play('theme', { loop: -1 });
+        this.sound.volume = 0.1;
+        this.sound.detune = 0;
+    }
+
+    update (time, delta)
+    {
+        // go back to main menu on click
+    }
 }
