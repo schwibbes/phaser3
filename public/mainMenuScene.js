7db8fa59d31aab03ab308f14ca5d4fdb7f8d362d
schwibbes2 <schwibbes2@gmail.com>
2021-05-22 23:34:41 +0200

    somewhat usable map editor

--- public/mainMenuScene.js
+++ public/mainMenuScene.js
@@ -4,10 +4,18 @@ class MainMenuScene extends Phaser.Scene {
         super('mainMenu');
     }
 
-    // preload () {}
+    preload () {
+        this.load.image('surligneur', 'assets/surligneur.png');
+        this.load.image('stylo-win', 'assets/stylo-win.png');
+
+    }
 
     create ()
     {
+        this.add.image(200, 500, 'stylo-win').setScale(1).setAlpha(0.2);
+        this.add.image(890, 500, 'surligneur').setScale(1).setAlpha(0.2);
+        this.add.text(50, 50, 'Carelessly jotting some thoughts, you have suddenly been trapped by an evil calligraphy master.\nIf you could only read his magic book.\nMaybe you could find your way back home?', { fill: '#0f0' }).setBackgroundColor('rgba(255, 0, 0, 0.5)')
+
         let xOffset = config.width / 2;
         let yOffset = config.height / 2 - 30;
         this.createButton(xOffset, yOffset, 'Start', 'game');
