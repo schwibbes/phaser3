831428f48f4527340dc057a9af3699f410a16b5d
Dennis The Menace <dennis.kopcan@gmail.com>
2021-05-22 13:01:59 +0200

    Add kana

--- riddleGenerator.js
+++ riddleGenerator.js
@@ -1,9 +1,9 @@
 var riddleGenerator = (function () {
 	var solutions = [
-		{ question: "Mensch", answer: "\u5165" },
-		{ question: "Loch", answer: "\u30ea" },
-		{ question: "Stärke", answer: "\u529b" },
-		{ question: "Disziplin", answer: "\u8ebe" },
+		{ question: "ki", answer: "\u304d" },
+		{ question: "pa", answer: "\u3071" },
+		{ question: "bo", answer: "\u307c" },
+		{ question: "ru", answer: "\u308b" },
 	];
 
 	var spawnPoints = [ 100, 400, 800, 650, 280, 487 ];
@@ -13,7 +13,7 @@ var riddleGenerator = (function () {
 	}
 
 	function getRandomChar(skip) {
-		var characters = "\u4eb0\u56f0\u4eb2\u5650\u5240\u55b0\u4eb6\u5140\u4eb8\u4eb9\u5390";
+		var characters = "\u3042\u304a\u3082\u3092\u3070\u3072\u3073\u3084\u3086\u3050\u305e\u306c\u306d\u308b\u307c\u3071\u304d";
 		var notUniqueAnswer = true;
 
 		while (notUniqueAnswer) {
